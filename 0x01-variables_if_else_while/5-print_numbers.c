70f9afb9c3d48b34901a4b44ccc06e3a9e7129dd
Benjamin Torfu <btorfu@proton.me>
2023-06-16 20:49:12 -0700

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+	while (a <10)
+	{
+		printf("%d",a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
