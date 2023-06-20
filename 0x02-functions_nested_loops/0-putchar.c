9e62e04243e077e067790a207d71b68bd8314847
Benjamin Torfu <btorfu@proton.me>
2023-06-20 13:14:47 -0700

    betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,14 @@
 
 /*
  * main - print _putchar using _putchar
- * Prints the word _putchar followed by a new line
+ *Description -  Prints the word _putchar followed by a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 char c[] = "_putchar";
-    
+
 int i;
 
 for (i = 0; i < 8; i++)
