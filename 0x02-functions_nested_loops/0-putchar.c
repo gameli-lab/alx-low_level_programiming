561bb016462f37b930f81c00e82791655cdf44db
Benjamin Torfu <btorfu@proton.me>
2023-06-20 13:10:11 -0700

    betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,15 @@
  */
 int main(void)
 {
-    char c[] = "_putchar";
-    int i;
+char c[] = "_putchar";
+    
+int i;
 
-    for (i = 0; i < 8; i++)
-    {
-        _putchar(c[i]);
-    }
-    _putchar('\n');
+for (i = 0; i < 8; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
 
-    return (0);
+return (0);
 }
