84d9660c7a458ceaf01af9c470e4f842ae240df6
Benjamin Torfu <btorfu@proton.me>
2023-06-20 13:06:06 -0700

    fixed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * main - print _putchar using _putchar
+ * Prints the word _putchar followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c[] = "_putchar";
+    int i;
+
+    for (i = 0; i < 8; i++)
+    {
+        _putchar(c[i]);
+    }
+    _putchar('\n');
+
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+source code files for nested while loops and functions
