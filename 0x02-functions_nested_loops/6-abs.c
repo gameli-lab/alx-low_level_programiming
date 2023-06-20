5b898a24ac0f5d8549fa8db11b3fd36648cdc6c7
Benjamin Torfu <btorfu@proton.me>
2023-06-20 16:52:52 -0700

    betty

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_abs - computes the absolute value of an integer
+ *
+ *@r: is the integer being tested by the function
+ *
+ *return: returns the absolute value of the integer
+ */
+
+int _abs(int r)
+{
+
+		if (r > 0)
+		{
+			return (+r);
+		}
+		else
+		{
+			return (-r);
+		}
+}
+
+	
