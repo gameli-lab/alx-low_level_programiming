3987d86f115168aee646eb8c5be3cba8df92f2fa
Benjamin Torfu <btorfu@proton.me>
2023-06-20 14:48:14 -0700

    betty please

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * print_alphabet_x10 - this prints the alphabet 10 times
+ *
+ *return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha, times;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+
+}
