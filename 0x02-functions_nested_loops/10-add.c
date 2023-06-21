f094faeeb407824f20835bad4c041700808bd44f
Benjamin Torfu <btorfu@proton.me>
2023-06-21 02:29:18 -0700

    sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,14 +4,16 @@
 /**
  *add - Adds the two integers
  *
+ *@i & @j: variables being added
+ *
  *Return: returns the results of the addition
  */
 
 int add(int i, int j)
 {
 	int k;
-	
+
 	k = i + j;
-	
+
 	return (k);
 }
