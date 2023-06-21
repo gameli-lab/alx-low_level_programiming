21aefd46835cdb6c6dc674c3106a91a6e7b2bf80
Benjamin Torfu <btorfu@proton.me>
2023-06-20 18:38:48 -0700

    betty

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints the minutes of the day
+ *
+ *Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar('0' + hrs / 10);
+			_putchar('0' + hrs % 10);
+			_putchar(':');
+			_putchar('0' + mins / 10);
+			_putchar('0' + mins % 10);
+			_putchar('\n');
+		}
+	}
+}
