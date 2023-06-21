fb38f81c377927cc47aa7d48bd70dad5735238d2
Benjamin Torfu <btorfu@proton.me>
2023-06-20 17:47:27 -0700

    betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
 /**
+ *print_last_digit - prints the last digit of a number
  *
+ *@r: the number who's last digit is being printed
  *
+ *@k: it stores the last digit
  *
- *
- *
+ *Return: returns the last digit
  */
 
-int print_last_digit(int)
+int print_last_digit(int r)
 {
-	
+	int k = r % 10;
+
+	_putchar(k);
+	return (k);
+}
