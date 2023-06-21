bb4e69d50ad0acbf5d648b9a0a33d38cd549b4c0
Benjamin Torfu <btorfu@proton.me>
2023-06-20 19:09:44 -0700

    betty

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ *add - Adds the two integers
+ *
+ *Return: Always 0 (Success)
+ */
+
+int add(int i, int j)
+{
+	int k = i + j;
+	_putchar('0' + k);
+	_putchar('\n');
+	return (0);
+}
