00cdb319e4403e28762dd86418d1520178be2c80
Benjamin Torfu <btorfu@proton.me>
2023-06-21 16:29:35 -0700

    sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - computes the sum of all multiples of 3 and 5
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main()
+{
+	int i, j = 1024, k = 0;
+
+	for (i = 1; i < j; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			k += i;
+		}
+	}
+	printf("%d\n", k);
+	return (0);
+}
