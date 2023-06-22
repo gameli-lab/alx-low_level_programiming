587988be12762bace3b63da60912e1d66a193fb8
Benjamin Torfu <btorfu@proton.me>
2023-06-22 13:51:48 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *mul - multiplies the variables
+ *
+ *@a: first variable to be multiplied
+ *
+ *@b: second variable to be multiplied
+ */
+
+int mul(int a, int b)
+{
+	int c;
+	
+	c = a * b;
+	return (c);
+}
