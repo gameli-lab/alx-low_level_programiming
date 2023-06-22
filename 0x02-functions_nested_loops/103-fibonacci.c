9817458106b815678cc5b913ce373694676edc46
Benjamin Torfu <btorfu@proton.me>
2023-06-21 18:15:17 -0700

    sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ *fibonacci - prints sum of even fibonacci numbers
+ *
+ *Return: returns the sum of the values
+ */
+
+unsigned long fibonacci()
+{
+	unsigned long sum = 0, a = 1, b = 2, c;
+
+	while (a <= 4000000)
+	{
+		if (a % 2 == 0)
+		{
+			sum += a;
+		}
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return (sum);
+}
+
+/**
+ *main - calls the fibonacci function
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long sum = fibonacci();
+	printf("%lu", sum);
+	return (0);
+}
