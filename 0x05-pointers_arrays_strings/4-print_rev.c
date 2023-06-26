44f7b6ee2cd1148684ada9e854288bf66c3eee6c
Benjamin Torfu <btorfu@proton.me>
2023-06-26 14:49:41 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_rev - prints characters in the reverse
+ *
+ *@s: string to be printed
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	char *z;
+	int l;
+
+	l = strlen(s);
+
+	z = s + l - 1;
+
+	while (z >= s)
+	{
+		printf("%c", *z);
+		z--;
+	}
+	printf("\n");
+}
