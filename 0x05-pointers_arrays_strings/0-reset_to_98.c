1118f89a50e1305231e3d3138124e297ae35a41d
Benjamin Torfu <btorfu@proton.me>
2023-06-26 01:36:53 -0700

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - this resets the value it is pointing to to 98
+ *
+ *@n: this is the pointer variable
+ *
+ *Ruturn: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 int main(void);
 void reset_to_98(int *n);
