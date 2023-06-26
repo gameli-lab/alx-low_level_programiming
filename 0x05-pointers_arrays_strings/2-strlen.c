820bdfe5b40839f66088915f3b6beb3af2f11344
Benjamin Torfu <btorfu@proton.me>
2023-06-26 03:05:41 -0700

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen - finds ths lenght of the string
+ *
+ *@*s: variable whose lenght is to be found
+ *
+ *Return: returns the lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	return(strlen(s));
+}
