61e3dc39e65cddffc08f1a4b2c43e9e3854a4e2a
Benjamin Torfu <btorfu@proton.me>
2023-06-26 14:14:46 -0700

    try

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts - prints strings to the stdout
+ *
+ *@str: string being printed
+ *
+ *Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
