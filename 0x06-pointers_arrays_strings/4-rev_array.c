df6de34cbaaf8834d29ad3a53205200975325787
Benjamin Torfu <btorfu@proton.me>
2023-06-29 14:44:56 -0700

    rev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *reverse_array - reverses the content of an array
+ *
+ *@a: the array in use
+ *
+ *@n: the number of members in the array
+ *
+ *Return: Always 0 (Success)
+ */
+
+void reverse_array(int *a, int n)
+{
+	int start, end, temp, i;
+
+	start = 0;
+	end = n - 1;
+
+	while (start < end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+
+		start++;
+		end--;
+	}
+}
