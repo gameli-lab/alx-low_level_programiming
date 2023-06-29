349a49f4211a56ebc9287202e38b44ddb6772ac2
Benjamin Torfu <btorfu@proton.me>
2023-06-29 14:24:54 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strcmp - compares s1 to s2
+ *
+ *@s1: first argument for comparison
+ *
+ *@s2: second argument for comparison
+ *
+ *Return: returns the result of the comparison
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
