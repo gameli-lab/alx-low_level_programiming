9eb9039953162b4006b5dd830fc7430f8a32a0da
Benjamin Torfu <btorfu@proton.me>
2023-07-03 14:14:58 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,10 +6,6 @@
  *
  *@s: address of area to be filled with constant bytes
  *
- *@b: constant bytes to be filled in memory
- *
- *@n: bytes of memory to be filled
- *
  *Return: returns the pointer to the memory area
  */
 
