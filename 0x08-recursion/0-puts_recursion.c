bb9f64621d29aac633d2c5c272d38366618a6956
Benjamin Torfu <btorfu@proton.me>
2023-07-05 16:38:50 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts_recursion - prints a string
+ *
+ *@a: pointer to the string to be printed
+ *
+ *Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
