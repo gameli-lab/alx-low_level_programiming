ccafa9e586d2195a6209f382ac0da3fc558d3bff
Benjamin Torfu <btorfu@proton.me>
2023-07-06 04:23:04 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,11 +3,13 @@
 #include <math.h>
 
 /**
- *is_prime_number - finds prime numbers
+ *is_prime - finds prime numbers
  *
  *@n: address to the integer
  *
- *Return: returns 1 for prime numbers otherwise 0
+ *@i: address to the divisor
+ *
+ *Return: returns check for next divisor
  */
 
 int is_prime(int n, int i)
@@ -27,6 +29,14 @@ int is_prime(int n, int i)
 	}
 	return (is_prime(n, i + 1));
 }
+
+/**
+ * is_prime_number - finds prime numbers
+ *
+ *@n: address to the number
+ *
+ *Return: returns 1 for prime numbers otherwise 0
+ */
 
 int is_prime_number(int n)
 {
