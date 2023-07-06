15390ca99b45f0b57206d492686df2152549607c
Benjamin Torfu <btorfu@proton.me>
2023-07-05 17:12:43 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *factorial - prints the factorial of n
+ *
+ *@n: address of the number
+ *
+ *Return: returns -1 if n < 0 to indicate error
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	return
+		n * factorial(n - 1);
+}
