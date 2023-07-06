40a2099b3970b2c09519889ce181b6224453742d
Benjamin Torfu <btorfu@proton.me>
2023-07-05 20:21:41 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,35 +3,56 @@
 #include <math.h>
 
 /**
- *sqrt_recursion - finds square root of a number
+ *sqrt_recursive - finds square root of a number
  *
  *@n: address to the number
  *
- *Return: returns the square root of the number otherwise -1
+ *@start: start of thr recursion
+ *
+ *@end: end of recursion
+ *
+ *Return: returns square_recursive
  */
 
-int _sqrt_recursion(int n)
+int _sqrt_recursive(int n, int start, int end) 
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0)
-	{
-		return (sqrt(n));
-	}
-	else
-	{
-		int res;
-		res = sqrt(n);
+   
+    if (start > end) 
+    {
+        return -1;
+    }
 
-		if (res * res == n)
-		{
-			return (res);
-		}
-		else
-		{
-			return (-1);
-		}
-	}
+    int mid = (start + end) / 2;
+    int square = mid * mid;
+
+    if (square == n) 
+    {
+        return mid; 
+    } 
+    else if (square < n) 
+    {
+        return _sqrt_recursive(n, mid + 1, end); 
+    } 
+    else 
+    {
+        return _sqrt_recursive(n, start, mid - 1);
+    }
+}
+
+/**
+ *_sqrt_recursion - finds the square root of numbers
+ *
+ *@n: address to the number
+ *
+ *Return: returns -1 for non natural square root numbers
+ */
+
+int _sqrt_recursion(int n) 
+{
+    if (n < 0) 
+    {
+        return -1; 
+    }
+
+    return _sqrt_recursive(n, 0, n);
 }
