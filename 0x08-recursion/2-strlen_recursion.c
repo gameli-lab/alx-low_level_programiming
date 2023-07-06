c064d9d4925eb57b016a3492455881f115b26185
Benjamin Torfu <btorfu@proton.me>
2023-07-05 17:24:00 -0700

    factorial

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  *_strlen_recursion - finds the length of a string
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,7 +12,15 @@
 int factorial(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
-	return
-		n * factorial(n - 1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
