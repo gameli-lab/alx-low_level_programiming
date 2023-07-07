f32700016631728f8fe19cd56fb331857715b25c
Benjamin Torfu <btorfu@proton.me>
2023-07-06 17:57:38 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,18 +14,17 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
+	if (*s1 == '\0'  && *s2 == '\0')
 	{
-		if (*s2 == '*')
-		{
-			return (1);
-		}
-		if (*s1 != *s2)
-		{
-			return (0);
-		}
-		s1++;
-		s2++;
+		return (1);
 	}
-	return (*s1 == *s2);
+	if (*s2 == '*')
+	{
+		return (1);
+	}
+	if (*s1 != *s2)
+	{
+		return (0);
+	}
+	return (wildcmp(s1 + 1, s2 + 1));
 }
