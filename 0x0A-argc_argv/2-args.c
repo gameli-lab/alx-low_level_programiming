cc5548cc94f7bf0470fb3969dbf4967b4f07abdc
Benjamin Torfu <btorfu@proton.me>
2023-07-09 12:46:54 -0700

    argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_all_argv - prints all argument values passed
+ *
+ *@argc: argument count
+ *
+ *@argv: argument value passed
+ *
+ *Return: Always 0 (Success)
+ */
+
+int print_all_argv(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
+
+/**
+ *main - prints the arguments passed
+ *
+ *@argc: counts the number of arguments passed
+ *
+ *@argv: holds the value of the argument passed
+ *
+ *Return: returns argv
+ */
+
+int main(int argc, char *argv[])
+{
+	print_all_argv(argc, argv);
+	return (argc);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,6 @@
 
 int myname(char *argv);
 int no_args(int argc);
-
+int print_all_argv(int argc, char *argv[]);
 
 #endif
