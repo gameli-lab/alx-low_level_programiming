6c0a2c973e12079685edff304ca8f7c9ced47012
Benjamin Torfu <btorfu@proton.me>
2023-07-11 10:19:55 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - duplicates the string
+ *
+ *@str: string to be duplicated
+ *
+ *Return: returns pointer to the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	s = strdup(str);
+	return (s);
+}
