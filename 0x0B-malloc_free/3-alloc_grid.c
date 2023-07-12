06b0d7a4611b4332fe14ddb008b1e348b9e51b81
Benjamin Torfu <btorfu@proton.me>
2023-07-11 20:20:00 -0700

    alloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	grid = (int **)malloc(height * sizeof(int));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	return (grid);
+}
