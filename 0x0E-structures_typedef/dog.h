fb8e94407a2d4b5fb83da1118e2479a4d736dae5
Benjamin Torfu <btorfu@proton.me>
2023-07-17 16:56:35 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,6 +10,13 @@ float age;
 char *owner;
 };
 
+struct dog_t
+{
+char *name;
+float age;
+char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
