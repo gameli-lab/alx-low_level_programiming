fba8ba3b2751e3840a3096c694b42dce24dcfa79
Benjamin Torfu <btorfu@proton.me>
2023-07-17 14:59:58 -0700

    pi

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -2,9 +2,5 @@
  *@file  my header file
  *@brief This file defines the macro PI as an abbreviation for the value 3.14159265359
  */
-#ifndef 1-PI_H
-#define 1-PI_H
 
 #define PI 3.14159265359
-
-#endif /* 1-PI_H */
