fe37cf2be5b54d329e9035e262d144203bccf11c
Benjamin Torfu <btorfu@proton.me>
2023-07-17 17:56:40 -0700

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ *free_dog - frees the allocated memories
+ *@d: pointer to the memory
+ *
+ *Return: returns nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	return;
+}
