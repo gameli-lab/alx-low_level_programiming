f957db8a534f52ab149fa6c151dadabeb1b0fb77
Benjamin Torfu <btorfu@proton.me>
2023-07-17 17:05:31 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,19 +3,12 @@
 #include <stdio.h>
 
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef dog dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-};
+};dog_t
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
