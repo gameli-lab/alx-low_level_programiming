9993d986d2d6873589f56a75890efb91ad2d3a9d
Benjamin Torfu <btorfu@proton.me>
2023-08-01 10:17:54 -0700

    list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *size_t listint_len - counts the number of elements in the linked list
+ *@h: header to the first node in the link
+ *
+ *Description: singly linked list node structure
+ *
+ *Return: returns the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h -> next;
+		count++;
+	}
+	return (count);
+}
