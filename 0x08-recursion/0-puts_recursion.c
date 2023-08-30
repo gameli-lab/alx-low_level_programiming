6fdaffbb4871cf2231a847b72ff2104c5dbb9eae
Benjamin Torfu <btorfu@proton.me>
2023-08-30 07:51:37 -0700

    rec

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,14 @@
 
 void _puts_recursion(char *s)
 {
-  if(*s == '\0')
-    {
-      _putchar('\0');
-      return;
-    }
+if(*s == '\0')
+{
+_putchar('\0');
+return;
+}
 
-  _putchar(*s);
-  _puts_recursion(s + 1);
+_putchar(*s);
+_putchar('\n');
+_puts_recursion(s + 1);
   
 }
