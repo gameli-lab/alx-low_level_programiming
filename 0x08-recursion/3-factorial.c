e5320caf18944b5d39e49a387bdce0216e4ad880
Benjamin Torfu <btorfu@proton.me>
2023-08-30 17:39:53 -0700

    fac

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *factorial - finds the factorial of a given number
+ *@n: factorial to find
+ *
+ *Return: factorial of the number
+ */
+
+int factorial(int n)
+{
+if (n == 0)
+{
+return (1);
+}
+return (n * factorial (n - 1));
+}
