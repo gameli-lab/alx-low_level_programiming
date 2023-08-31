b482228e4da7a8aa0e7cdd9b2064c3454e684265
Benjamin Torfu <btorfu@proton.me>
2023-08-30 22:43:07 -0700

    pal

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ *checker - checks for palindromes
+ *@s: string to check
+ *@start: starting point
+ *@end: end point
+ *
+ *Return: the results
+ */
+
+int checker(char *s, int start, int end)
+{
+if (start >= end)
+{
+return (1);
+}
+if (s[start] != s[end])
+{
+return (0);
+}
+return (checker(s, start + 1, end - 1));
+}
+
+/**
  *is_palindrome - check if a string is a palindrome
  *@s: string to check
  *
@@ -9,16 +31,9 @@
 
 int is_palindrome(char *s)
 {
-int len, i;
+int len;
 
 len = strlen(s);
 
-for (i = 0; i < len / 2; i++)
-{
-if (s[i] != s[len - 1 -i])
-{
-return (0);
-}
-}
-return (1);
+ return (checker(s, 0, len - 1);
 }
