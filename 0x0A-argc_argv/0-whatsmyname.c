6c93e42c1200a981bf6f7ba2ce78376f0ea6a1a0
Benjamin Torfu <btorfu@proton.me>
2023-09-01 17:27:14 -0700

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- *main - entry point
- *@argc: argument count
- *@argv: argument vectro
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vectro
  *
  * Return: 0 on success
  * */
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n",argv[0]);
-return (0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
