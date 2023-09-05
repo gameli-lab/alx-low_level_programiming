1b3695229893dff0900ad4f3c3eec5b0931157e6
Benjamin Torfu <btorfu@proton.me>
2023-09-05 10:46:06 -0700

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * alloc_grid - allocates memory to a 2D array
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: a pointer to the array otherwise NULL
+ */
+
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **grid;
+
+	if ((height < 1) || (weight < 1))
+	{
+		return (NULL);
+	}
+
+	grid = (int **)malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int))
+			if (grid[i] == NULL)
+			{
+				for (j = 0; j < i; j++)
+				{
+					free (grid[i]);
+				}
+			free (grid);
+			return (NULL);
+			}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
