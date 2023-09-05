cdbc2f88d1308475d24670596ef89edb50b0fdea
Benjamin Torfu <btorfu@proton.me>
2023-09-05 15:01:37 -0700

    hhmmm

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,8 +10,9 @@
 
 char *argstostr(int ac, char **av)
 {
-	int totalLen = 0;
-	int index, i;
+	size_t totalLen = 0;
+	int index = 0;
+	int i;
 	char *result;
 
 	if ((ac == 0) || (av == NULL))
@@ -21,29 +22,26 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		totalLen += strlen(av[i] + 1);
+		if (av[i] != NULL)
+		{
+			totalLen += strlen(av[i]) + 1;
+		}
 	}
-
-	result = (char *)malloc(totalLen);
-
+	result = (char *)malloc((totalLen + 1) * sizeof(char));
 	if (result == NULL)
 	{
 		return (NULL);
 	}
-
-	index = 0;
-
 	for (i = 0; i < ac; i++)
 	{
-		strcpy(result + index, av[i]);
-		index += strlen(av[i]);
-
-		if (i < ac - 1)
+		if (av[i] != NULL)
 		{
+			strcpy(result + index, av[i]);
+			index += strlen(av[i]);
 			result[index] = '\n';
 			index++;
 		}
 	}
-	result[index] = '\0';
+	result[totalLen] = '\0';
 	return (result);
 }
