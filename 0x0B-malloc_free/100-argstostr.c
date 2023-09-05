1592bf92e01ea4527a82443711eab75116404ae3
Benjamin Torfu <btorfu@proton.me>
2023-09-05 12:27:57 -0700

    argconcat

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the arguments
+ * @ac:first argument
+ * @av: secont argument
+ *
+ * Return: a pointer to the new string otherwise NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int totalLen = 0;
+	int index, i;
+	char *result;
+
+	if ((ac == 0) || (av == NULL))
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		totalLen += strlen(av[i]);
+	}
+	totalLen += ac - 1;
+
+	result = (char *)malloc(totalLen + 1);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	index = 0;
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(result + index, av[i]);
+		index += strlen(av[i]);
+
+		if (i < ac - 1)
+		{
+			result[index] = '\n';
+			index++;
+		}
+	}
+	result[index] = '\0';
+	return (result);
+}
+	
