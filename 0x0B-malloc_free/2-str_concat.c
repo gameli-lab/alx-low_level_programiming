550c1751f9f6912bb33dd69cd43e56e2de9f0c75
Benjamin Torfu <btorfu@proton.me>
2023-09-05 03:33:58 -0700

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: a pointer to the new memory otherwise NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	size_t len1, len2;
+
+	if (s1 == NULL)
+	{
+		s1 == "";
+	}
+	if (s2 == NULL)
+	{
+		s2 == "";
+	}
+
+	concat = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(concat, s1);
+	strcat(concat, s2);
+
+	return (concat);
+}
