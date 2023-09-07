50fcfffeddbeaafd206be0bfe2328d68f5363770
Benjamin Torfu <btorfu@proton.me>
2023-09-07 08:46:37 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory to an array
+ * @nmemb: size of the array
+ * @size: size of each member
+ *
+ * Return: return a pointer to the allocated memory or NULL on erreo
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	size_t totalSize;
+
+	if ((nmemb == 0) || (size == 0))
+	{
+		return (NULL);
+	}
+
+	totalSize =(size_t) nmemb * size;
+
+	void *mem = malloc(totalSize);
+
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
+	memset(mem, 0, totalSize);
+
+	return (mem);
+}
+
