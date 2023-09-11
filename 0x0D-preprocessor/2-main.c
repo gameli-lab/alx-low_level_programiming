911f0631ce52287184830380950f4bc7bdc6616b
Benjamin Torfu <btorfu@proton.me>
2023-09-11 06:37:06 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -13,7 +12,7 @@ int main(int argc, char* argv[])
 {
 	while(argc)
 	{
-		printf("%d\n", argv[]);
+		printf("%s\n", argv[0]);
 		return (0);
 	}
 }
