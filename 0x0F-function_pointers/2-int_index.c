03778280cf6a9b64068824252fce74f871eb9808
Benjamin Torfu <btorfu@proton.me>
2023-09-13 16:59:59 -0700

    point

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,18 +13,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-	{
-		return (-1);
-	}
-	if (size < 1)
+	if (array == NULL || cmp == NULL || size < 1)
 	{
 		return (-1);
 	}
 
 	for (i = 0; i < size; i++)
 	{
-		cmp(array[i]);
-		return (i);
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
 	}
+	return (-1);
 }
