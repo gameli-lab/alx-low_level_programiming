dba251c651e2f382a43fd53368b25715f02f1ffd
Benjamin Torfu <btorfu@proton.me>
2023-09-14 20:16:59 -0700

    num

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: separates the numbers
+ * @n: constant passed
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list numbers;
+	va_start(numbers, n);
+
+	if (separator == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	printf("%d", va_arg(numbers, int));
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s%d", separator, va_arg(numbers, int));
+	}
+	printf("\n");
+	return;
+}
