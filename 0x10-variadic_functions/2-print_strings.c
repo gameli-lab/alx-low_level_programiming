5167338d35791063ad022953fbc39e91d3af458b
Benjamin Torfu <btorfu@proton.me>
2023-09-14 21:53:17 -0700

    name

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,27 +10,32 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	char* str;
 	unsigned int x;
 	va_list strings;
 	va_start(strings, n);
-	char* str = va_arg(strings, char*);
 
 	if (separator == NULL)
 	{
 		printf("\n");
 		return;
 	}
-	if (str == NULL)
+
+	for (x = 0; x < n; x++)
 	{
-		str = "nil";
-	}
-
-	printf("%s", str);
-
-	for (x = 1; x < n; x++)
-	{
-
-		printf("%s%s", separator, str);
+		str = va_arg(strings, char*);
+		if (str == NULL)
+		{
+			str = "(nil)";
+		}
+		if (x == 0)
+		{
+			printf("%s", str);
+		}
+		else
+		{
+			printf("%s%s", separator, str);
+		}
 	}
 	va_end(strings);
 	printf("\n");
