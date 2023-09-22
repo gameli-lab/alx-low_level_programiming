82e61f894b0f400b5de097a23b763c65fc461979
Benjamin Torfu <btorfu@proton.me>
2023-09-22 05:20:18 -0700

    printing list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,16 @@
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+int count = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		count++;
-		h = h->next;
-	}
-
-	return (count);
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+count++;
+h = h->next;
+}
+return (count);
 }
