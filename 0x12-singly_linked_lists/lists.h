9742712153328f2bdd1c79a661727b683bf1f4c6
Benjamin Torfu <btorfu@proton.me>
2023-09-22 22:19:32 -0700

    updated the prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 /**
@@ -24,6 +25,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 
 #endif /* LISTS_H */
