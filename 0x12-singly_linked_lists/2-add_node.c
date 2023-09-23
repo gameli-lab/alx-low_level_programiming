fa5419a56a08dec73e8ebb6e113ee0d2218a9a1c
Benjamin Torfu <btorfu@proton.me>
2023-09-22 20:26:24 -0700

    add node beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,6 +10,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	unsigned int len = 0;
 
 	new = malloc(sizeof(list_t));
 
@@ -23,7 +24,12 @@ list_t *add_node(list_t **head, const char *str)
 		free(new);
 		return (NULL);
 	}
+	while (str[len])
+		len++;
+	new->len = len;
+
 	new->next = *head;
+
 	*head = new;
 
 	return (new);
