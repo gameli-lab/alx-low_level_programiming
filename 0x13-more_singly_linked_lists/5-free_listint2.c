9ed81ca21588608ef07d3e375bffeb5779ba11cf
Benjamin Torfu <btorfu@proton.me>
2023-09-25 18:09:24 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the list
+ * @head: pointer to pointer to the head of the list
+ * Return: Nothing
+ */
+
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
