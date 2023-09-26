e4e9fdd8af6d45b2e3fc7a7618b1de0dadbebbba
Benjamin Torfu <btorfu@proton.me>
2023-09-25 18:58:25 -0700

    task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,14 +11,10 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 
 	if (*head == NULL)
-	{
 		return (0);
-	}
-	else
-	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
-		return (*head);
-	}
+
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return (*head);
 }
