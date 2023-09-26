c8de233c9f27a667abfee6ab89e169d90a3f36e4
Benjamin Torfu <btorfu@proton.me>
2023-09-25 18:48:08 -0700

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - dels first node
+ * @head: pointer to pointer to the head
+ * Return: data of the head otherwise 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+	temp = *head;
+
+	*head = (*head)->next;
+
+	free(temp);
+
+	return((*head)->n);
+}
