a5d3233c89c2ee981d587f7ee516b026dc03c846
Benjamin Torfu <btorfu@proton.me>
2023-09-25 18:15:49 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,10 +11,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (*head == NULL || head == NULL)
+		return (NULL);
+
 	while (*head)
 	{
 		temp = *head;
-		*head = (*head)->next;
+		*head = (*(head))->next;
 		free(temp);
 	}
 	*head = NULL;
