bbdd3eabb5c4108faf9af36174ff010d1387e746
Benjamin Torfu <btorfu@proton.me>
2023-09-26 14:06:51 -0700

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums all the data in the list
+ * @head: pointer to the head of the list
+ * Return: sum of all the data
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head)
+	{
+		i += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
