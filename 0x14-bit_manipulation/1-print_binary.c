32b888495103700e65f4f21edc60ade2fe420747
Benjamin <btorfu@proton.me>
2023-09-28 22:17:24 +0000

    binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,7 +5,7 @@
  * @n: argument passed
  * Return: Nothing
  */
-
+/**
 void print_binary(unsigned long int n)
 {
 	int pos, bit;
@@ -24,6 +24,27 @@ void print_binary(unsigned long int n)
 		bit = (n >> pos) & 1;
 		_putchar(bit + '0');
 		pos--;
+	}
+	_putchar('\n');
+}
+*/
+
+void print_binary(unsigned long int n)
+{
+	int size = sizeof(unsigned long int) * 8;
+	unsigned long int mask = 1UL << (size - 1);
+
+	while (mask > 0) 
+	{
+		if (n & mask) 
+		{
+			_putchar('1');
+		}
+		else 
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
 	}
 	_putchar('\n');
 }
