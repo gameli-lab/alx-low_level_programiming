e4c0bc4e3054cecd2473cf541bb6fdcdcc5c9c00
Benjamin Torfu <btorfu@proton.me>
2023-09-28 06:51:06 -0700

    bin-to-int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * int binary_to_uint - converts binary to unsigned int
+ * @b: pointer to a string
+ * Return: the converted number otherwise 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i;
+
+	if (b != NULL )
+		return (0);
+
+	while (b[i] == '0' || b[i] == '1')
+	{
+		num <<= 1;
+		num += b[i] - '0';
+		i++;
+	}
+	return (num);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Bit manipulation priject files
