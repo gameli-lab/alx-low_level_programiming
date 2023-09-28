713a32aac0f91a460149e3594049581bb32063a1
Benjamin Torfu <btorfu@proton.me>
2023-09-28 07:04:15 -0700

    bin-to-int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int binary_to_uint - converts binary to unsigned int
+ * binary_to_uint - converts binary to unsigned int
  * @b: pointer to a string
  * Return: the converted number otherwise 0
  */
