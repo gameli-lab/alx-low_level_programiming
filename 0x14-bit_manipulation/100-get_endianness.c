3ea52637e667883c5f7727756a70369c3d655cdc
root <root@localhost.localdomain>
2023-10-27 05:19:37 +0000

    end

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - gets the endiannes of the system
+ * Return:  the endianness
+ */
+
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	unsigned char *my = (unsigned char *) & num;
+
+	return ((int) * my);
