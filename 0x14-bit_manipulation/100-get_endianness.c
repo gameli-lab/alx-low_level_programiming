16ecb843b41dd823e0458039a56df964af8e47a9
Benjamin <btorfu@proton.me>
2023-10-28 05:59:45 +0000

    end

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * get_endianness - gets the endiannes of the system
+ * get_endianness - gets the endiannes of the syst
  * Return: the endianness
  */
 
 int get_endianness(void)
 {
 	unsigned int num = 1;
+
 	unsigned char *my = (unsigned char *) & num;
 
 	return ((int) * my);
