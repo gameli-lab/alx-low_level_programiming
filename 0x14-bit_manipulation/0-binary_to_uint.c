6b051e16fae5387db4d2448e1b0667c2d9e51de5
Benjamin Torfu <btorfu@proton.me>
2023-11-24 05:58:33 +0000

    flip

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int res = 0, i;
+	unsigned int res = 0, i;
 
-        if (!b)
-                return (0);
-        for (i = 0; b[i]; i++)
-        {
-                if (b[i] < '0' || b[i] > '1')
-                        return (0);
-                res = 2 * res + (b[i] - '0');
-        }
-        return (res);
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		res = 2 * res + (b[i] - '0');
+	}
+	return (res);
 }
 
