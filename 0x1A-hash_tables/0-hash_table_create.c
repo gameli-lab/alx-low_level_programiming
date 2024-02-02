00f3acf0692835bb63edb2dd87a2b95518266c07
Benjamin Torfu <btorfu@proton.me>
2024-02-02 04:39:25 +0000

    key

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t* table;
+	unsigned long int i;
+       
+	table = malloc(sizeof(hash_table_t));
+	table->size = size;
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
