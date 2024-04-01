8d31889692a9327dc6acc5bf16470af5b044a603
Benjamin Torfu <btorfu@proton.me>
2024-04-01 16:10:40 +0000

    perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-'''
-This is the island grid module '''
+''' This is the island grid module '''
 
 
 def island_perimeter(grid):
-    '''
-    The island perimeter method '''
+    '''    The island perimeter method '''
     perimeter, rows, cols = 0, len(grid), len(grid[0])
 
     dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
