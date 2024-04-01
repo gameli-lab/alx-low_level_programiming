2ee5675affcaa19a46ac66069ca087e80b2f1a8a
Benjamin Torfu <btorfu@proton.me>
2024-04-01 16:28:09 +0000

    perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@
 This is the island perimeter module
 """
 
+
 def island_perimeter(grid):
     """
     This is the method for finding the perimeter of the island
@@ -18,6 +19,6 @@ def island_perimeter(grid):
                 for dr, dc in dirs:
                     nr, nc = r + dr, c + dc
                     if nr < 0 or nr >= rows or nc < 0 or nc >= cols or\
-                    grid[nr][nc] == 0:
+                            grid[nr][nc] == 0:
                         per += 1
     return (per)
