ba3813a2da449620c3108d32b43b238d7f4280b7
Benjamin Torfu <btorfu@proton.me>
2024-05-09 09:35:24 +0000

    linear

--- 0x1E-search_algorithms/0-main.c
+++ 0x1E-search_algorithms/0-main.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+/* #include <stdio.h>
 #include <stdlib.h>
 #include "search_algos.h"
 
-/**
+**
  * main - Entry point
  *
  * Return: Always EXIT_SUCCESS
- */
+ *
 int main(void)
 {
     int array[] = {
@@ -18,4 +18,22 @@ int main(void)
     printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
     printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
     return (EXIT_SUCCESS);
+} */
+
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int array[10] = {
+		8, 1, 42, 3, 4, 42, 6, 7, -1, 9
+	};
+
+	printf("Found at index: %d\n", linear_search(array, 10, 3));
+	return (0);
 }
